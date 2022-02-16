1ce025ca51b3d85143b834e8f37ad6c9842fe47a
DjDeveloperr <djsandhu2005@gmail.com>
2022-02-16 15:09:38 +0530

    cleanup postSetup

--- src/util.ts
+++ src/util.ts
@@ -1,12 +1,22 @@
 export const encoder = new TextEncoder();
 export const decoder = new TextDecoder();
 
-// On Unix based systems, we need to supply dlopen with RTLD_GLOBAL
-// but Deno.dlopen does not support passing that flag. So we'll open
-// libc and use its dlopen to open with RTLD_LAZY | RTLD_GLOBAL to
-// allow subsequently loaded shared libraries to be able to use symbols
-// from Python C API.
+const libDlDef = {
+  dlopen: {
+    parameters: ["pointer", "i32"],
+    result: "pointer",
+  },
+} as const;
+
+/**
+ * On Unix based systems, we need to supply dlopen with RTLD_GLOBAL
+ * but Deno.dlopen does not support passing that flag. So we'll open
+ * libc and use its dlopen to open with RTLD_LAZY | RTLD_GLOBAL to
+ * allow subsequently loaded shared libraries to be able to use symbols
+ * from Python C API.
+ */
 export function postSetup(lib: string) {
+  let libdl: Deno.DynamicLibrary<typeof libDlDef>;
   if (Deno.build.os === "linux") {
     const libc = Deno.dlopen(`libc.so.6`, {
       gnu_get_libc_version: { parameters: [], result: "pointer" },
@@ -16,26 +26,22 @@ export function postSetup(lib: string) {
     );
     const glibcVersion = parseFloat(ptrView.getCString());
 
-    const libdl = Deno.dlopen(
+    libdl = Deno.dlopen(
       // starting with glibc 2.34, libdl is merged into libc
       glibcVersion >= 2.34 ? `libc.so.6` : `libdl.so.2`,
-      {
-        dlopen: {
-          parameters: ["pointer", "i32"],
-          result: "pointer",
-        },
-      },
+      libDlDef,
     );
-    libdl.symbols.dlopen(cstr(lib), 0x00001 | 0x00100);
   } else if (Deno.build.os === "darwin") {
-    const libc = Deno.dlopen(`libc.dylib`, {
+    libdl = Deno.dlopen(`libc.dylib`, {
       dlopen: {
         parameters: ["pointer", "i32"],
         result: "pointer",
       },
     });
-    libc.symbols.dlopen(cstr(lib), 0x00001 | 0x00100);
+  } else {
+    return;
   }
+  libdl.symbols.dlopen(cstr(lib), 0x00001 | 0x00100);
 }
 
 /**
